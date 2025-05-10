8e945a3f9cd5689cacace67cae75c3d57a6b5de2
Semih Aybatan <semihaybatan@gmail.com>
2025-05-10 18:32:36 +0300

    Role entity created

--- src/main/java/com/example/authservice/entity/User.java
+++ src/main/java/com/example/authservice/entity/User.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -21,4 +24,12 @@ public class User {
     private String username;
 
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
