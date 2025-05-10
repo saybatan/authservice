366d96ea0e9f1c6ac2387e47f27d923e9ad7a5ac
Semih Aybatan <semihaybatan@gmail.com>
2025-05-10 17:24:19 +0300

    swagger-ui & controller tests

---
+++ src/main/java/com/example/authservice/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.authservice.controller;
+
+import com.example.authservice.entity.User;
+import com.example.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping
+    public User saveUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
